69213539fb709cf8dcc97a2a3d0ffa03bf85762e
Jaagruthi Musinada <jaagruthimusinada@gmail.com>
2026-04-22 19:59:14 +0530

    fix: enable trust proxy for production oauth

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const authRoutes = require('./routes/auth');
 const taskRoutes = require('./routes/tasks');
 
 const app = express();
+app.set('trust proxy', 1); // Enable trusting the Render proxy
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
